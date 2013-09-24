49656b9733e5c55390a8ee2fea98911b9607131a
apocas <petermdias@gmail.com>
2013-09-24 21:58:31 +0100

    refactoring

--- main.js
+++ main.js
@@ -14,27 +14,15 @@ var stream = net.connect({port: 8080, host: 'localhost'}, function() {
     case 'run':
       stream.write(JSON.stringify({command: 'run', language: args.shift(), repository: args.shift(), domain: args.shift()}));
       break;
-    case 'test':
-      stream.write(JSON.stringify({command: 'run', language: 'php', repository: 'https://github.com/apocas/helloworld-php'}));
-      break;
-    case 'start':
-      stream.write(JSON.stringify({command: 'start', id: args.shift()}));
-      break;
-    case 'stop':
-      stream.write(JSON.stringify({command: 'stop', id: args.shift()}));
-      break;
-    case 'delete':
-    case 'remove':
-      stream.write(JSON.stringify({command: 'remove', id: args.shift()}));
-      break;
     case 'list':
       stream.write(JSON.stringify({command: 'list'}));
       break;
+    case 'start':
+    case 'stop':
+    case 'remove':
     case 'logs':
-      stream.write(JSON.stringify({command: 'logs', id: args.shift()}));
-      break;
     case 'info':
-      stream.write(JSON.stringify({command: 'info', id: args.shift()}));
+      stream.write(JSON.stringify({command: command, id: args.shift()}));
       break;
     default:
       console.log('Unknown command.');
